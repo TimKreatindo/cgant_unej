d94706500b7a8dd846d8c392ec2aac6de6112028
didik setya <didiksetyaone0@yahoo.com>
2025-03-05 09:10:48 +0700

    first commit : setup awal

---
+++ application/views/admin/template.php
@@ -0,0 +1,218 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Home | Dashboardkit Dashboard Template</title>
+    <!-- [Meta] -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=0,minimal-ui">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description"
+        content="Dashboardkit is trending dashboard template made using Bootstrap 5 design framework. Dashboardkit is available in Bootstrap, React, CodeIgniter, Angular,  and .net Technologies.">
+    <meta name="keywords"
+        content="Bootstrap admin template, Dashboard UI Kit, Dashboard Template, Backend Panel, react dashboard, angular dashboard">
+    <meta name="author" content="Codedthemes">
+    <!-- [Favicon] icon -->
+    <link rel="icon" href="<?= base_url('assets/img/web/logo.png') ?>" type="image/x-icon">
+    <!-- [Google Font : Public Sans] icon -->
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@700..700&display=swap" rel="stylesheet">
+    <!-- [phosphor Icons] https://phosphoricons.com/ -->
+    <link rel="stylesheet" href="<?= base_url('template/admin') ?>/fonts/phosphor/duotone/style.css">
+    <!-- [Tabler Icons] https://tablericons.com -->
+    <link rel="stylesheet" href="<?= base_url('template/admin') ?>/fonts/tabler-icons.min.css">
+    <!-- [Feather Icons] https://feathericons.com -->
+    <link rel="stylesheet" href="<?= base_url('template/admin') ?>/fonts/feather.css">
+    <!-- [Font Awesome Icons] https://fontawesome.com/icons -->
+    <link rel="stylesheet" href="<?= base_url('template/admin') ?>/fonts/fontawesome.css">
+    <!-- [Material Icons] https://fonts.google.com/icons -->
+    <link rel="stylesheet" href="<?= base_url('template/admin') ?>/fonts/material.css">
+    <!-- [Template CSS Files] -->
+    <link rel="stylesheet" href="<?= base_url('template/admin') ?>/css/style.css" id="main-style-link">
+</head>
+
+<body data-pc-preset="preset-1" data-pc-sidebar-theme="dark" data-pc-header-theme="light" data-pc-sidebar-caption="true"
+    data-pc-direction="ltr" data-pc-theme="light">
+    <!-- [ Pre-loader ] start -->
+    <div class="loader-bg">
+        <div class="pc-loader">
+            <div class="loader-fill"></div>
+        </div>
+    </div>
+    <!-- [ Pre-loader ] End -->
+    <!-- [ Sidebar Menu ] start -->
+    <nav class="pc-sidebar">
+        <div class="navbar-wrapper">
+            <div class="m-header">
+                <a href="" class="b-brand text-primary text-center">
+                    <img src="<?= base_url('assets/img/web/logo.png') ?>" alt="logo image" class="logo-lg" width="50px">
+                </a>
+            </div>
+            <div class="navbar-content">
+                <ul class="pc-navbar">
+                    <li class="pc-item pc-caption"><label>Navigation</label></li>
+                    <li class="pc-item"><a href="../dashboard/index.html" class="pc-link"><span class="pc-micon"><i
+                                    class="material-icons-two-tone">home</i> </span><span
+                                class="pc-mtext">Dashboard</span></a></li>
+
+
+                    <li class="pc-item pc-hasmenu">
+                        <a href="#!" class="pc-link"><span class="pc-micon"><i
+                                    class="material-icons-two-tone">list_alt</i></span> <span class="pc-mtext">Menu
+                                levels</span> <span class="pc-arrow"><i data-feather="chevron-right"></i></span></a>
+                        <ul class="pc-submenu">
+                            <li class="pc-item"><a class="pc-link" href="#!">Level 2.1</a></li>
+                            <li class="pc-item pc-hasmenu">
+                                <a href="#!" class="pc-link">Level 2.2 <span class="pc-arrow"><i
+                                            data-feather="chevron-right"></i></span></a>
+                                <ul class="pc-submenu">
+                                    <li class="pc-item"><a class="pc-link" href="#!">Level 3.1</a></li>
+                                    <li class="pc-item"><a class="pc-link" href="#!">Level 3.2</a></li>
+                                    <li class="pc-item pc-hasmenu">
+                                        <a href="#!" class="pc-link">Level 3.3 <span class="pc-arrow"><i
+                                                    data-feather="chevron-right"></i></span></a>
+                                        <ul class="pc-submenu">
+                                            <li class="pc-item"><a class="pc-link" href="#!">Level 4.1</a></li>
+                                            <li class="pc-item"><a class="pc-link" href="#!">Level 4.2</a></li>
+                                        </ul>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li class="pc-item pc-hasmenu">
+                                <a href="#!" class="pc-link">Level 2.3 <span class="pc-arrow"><i
+                                            data-feather="chevron-right"></i></span></a>
+                                <ul class="pc-submenu">
+                                    <li class="pc-item"><a class="pc-link" href="#!">Level 3.1</a></li>
+                                    <li class="pc-item"><a class="pc-link" href="#!">Level 3.2</a></li>
+                                    <li class="pc-item pc-hasmenu">
+                                        <a href="#!" class="pc-link">Level 3.3 <span class="pc-arrow"><i
+                                                    data-feather="chevron-right"></i></span></a>
+                                        <ul class="pc-submenu">
+                                            <li class="pc-item"><a class="pc-link" href="#!">Level 4.1</a></li>
+                                            <li class="pc-item"><a class="pc-link" href="#!">Level 4.2</a></li>
+                                        </ul>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- [ Sidebar Menu ] end -->
+    <!-- [ Header Topbar ] start -->
+    <header class="pc-header">
+        <div class="header-wrapper">
+            <!-- [Mobile Media Block] start -->
+            <div class="me-auto pc-mob-drp">
+                <ul class="list-unstyled">
+                    <!-- ======= Menu collapse Icon ===== -->
+                    <li class="pc-h-item pc-sidebar-collapse"><a href="#" class="pc-head-link ms-0" id="sidebar-hide"><i
+                                class="material-icons-two-tone">menu</i></a></li>
+                    <li class="pc-h-item pc-sidebar-popup"><a href="#" class="pc-head-link ms-0" id="mobile-collapse"><i
+                                class="material-icons-two-tone">menu</i></a></li>
+                </ul>
+            </div>
+            <!-- [Mobile Media Block end] -->
+            <div class="ms-auto">
+                <ul class="list-unstyled">
+
+                    <li class="dropdown pc-h-item header-user-profile">
+                        <a class="pc-head-link dropdown-toggle arrow-none me-0" data-bs-toggle="dropdown" href="#"
+                            role="button" aria-haspopup="false" data-bs-auto-close="outside" aria-expanded="false"><img
+                                src="../assets/images/user/avatar-2.jpg" alt="user-image" class="user-avtar"> <span
+                                class="ms-2"><span class="user-name">Joseph William</span> <span
+                                    class="user-desc">Administrator</span></span></a>
+                        <div class="dropdown-menu dropdown-user-profile dropdown-menu-end pc-h-dropdown">
+                            <div class="dropdown-header d-flex align-items-center justify-content-between">
+                                <h4 class="m-0">Profile</h4>
+                            </div>
+                            <div class="dropdown-body">
+                                <div class="profile-notification-scroll position-relative"
+                                    style="max-height: calc(100vh - 225px)">
+                                    <ul class="list-group list-group-flush w-100">
+                                        <li class="list-group-item">
+                                            <div class="d-flex align-items-center">
+                                                <div class="flex-shrink-0"><img src="../assets/images/user/avatar-2.jpg"
+                                                        alt="user-image" class="wid-50 rounded-circle"></div>
+                                                <div class="flex-grow-1 mx-3">
+                                                    <h5 class="mb-0">Carson Darrin</h5>
+                                                    <a class="text-sm link-secondary"
+                                                        href="mailto:carson.darrin@company.io">carson.darrin@company.io</a>
+                                                </div>
+                                            </div>
+                                        </li>
+                                        <li class="list-group-item">
+                                            <a href="#" class="dropdown-item"><span class="d-flex align-items-center"><i
+                                                        class="fas fa-user-edit"></i> <span>Edit Profile</span>
+                                                </span></a>
+
+                                            <a href="#" class="dropdown-item"><span class="d-flex align-items-center"><i
+                                                        class="fas fa-key"></i> <span>Edit Password</span>
+                                                </span></a>
+
+                                            <a href="#" class="dropdown-item"><span class="d-flex align-items-center"><i
+                                                        class="fas fa-sign-out-alt"></i> <span>Keluar</span>
+                                                </span></a>
+
+
+
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+
+                </ul>
+            </div>
+        </div>
+    </header>
+    <!-- [ Header ] end -->
+    <!-- [ Main Content ] start -->
+    <div class="pc-container">
+        <div class="pc-content">
+
+        </div>
+    </div>
+    <!-- [ Main Content ] end -->
+    <footer class="pc-footer">
+        <div class="footer-wrapper container-fluid">
+            <div class="row">
+                <div class="col my-1">
+                    <p class="m-0">&copy 2025 team </p>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- Required Js -->
+
+    <script src="<?= base_url('template/admin') ?>/js/plugins/popper.min.js"></script>
+    <script src="<?= base_url('template/admin') ?>/js/plugins/simplebar.min.js"></script>
+    <script src="<?= base_url('template/admin') ?>/js/plugins/bootstrap.min.js"></script>
+    <script src="<?= base_url('template/admin') ?>/js/fonts/custom-font.js"></script>
+    <script src="<?= base_url('template/admin') ?>/js/pcoded.js"></script>
+    <script src="<?= base_url('template/admin') ?>/js/plugins/feather.min.js"></script>
+
+
+    <script src="<?= base_url('template/client') ?>/extensions/jquery/jquery.min.js"></script>
+    <?php
+        if(isset($js)){
+            foreach($js as $j){
+                echo '<script src="'.base_url('assets/js/admin/').$j.'"></script>';
+            }
+        }
+    ?>
+
+
+    <script>
+    layout_change('light');
+    layout_sidebar_change('dark');
+    change_box_container('false');
+    layout_caption_change('true');
+    layout_rtl_change('false');
+    preset_change('preset-1');
+    </script>
+</body>
+
+</html>
---
+++ application/views/client/template.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard - Mazer Admin Dashboard</title>
+
+    <link rel="icon" href="<?= base_url('assets/img/web/logo.png') ?>" type="image/x-icon">
+
+    <link rel="stylesheet" href="<?= base_url('template/client/') ?>compiled/css/app.css">
+    <link rel="stylesheet" href="<?= base_url('template/client/') ?>compiled/css/app-dark.css">
+    <link rel="stylesheet" href="<?= base_url('template/client/') ?>compiled/css/iconly.css">
+</head>
+
+<body>
+    <script src="<?= base_url('template/client/') ?>static/js/initTheme.js"></script>
+    <div id="app">
+        <div id="sidebar">
+            <div class="sidebar-wrapper active">
+                <div class="sidebar-header position-relative">
+                    <div class="d-flex justify-content-between align-items-center">
+                        <div class="logo w-100">
+                            <a href=""><img width="70px" style="height: 60px"
+                                    src="<?= base_url('assets/img/web/logo.png') ?>" alt="Logo"></a>
+                        </div>
+                        <div class="theme-toggle d-flex gap-2  align-items-center mt-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                                aria-hidden="true" role="img" class="iconify iconify--system-uicons" width="20"
+                                height="20" preserveAspectRatio="xMidYMid meet" viewBox="0 0 21 21">
+                                <g fill="none" fill-rule="evenodd" stroke="currentColor" stroke-linecap="round"
+                                    stroke-linejoin="round">
+                                    <path
+                                        d="M10.5 14.5c2.219 0 4-1.763 4-3.982a4.003 4.003 0 0 0-4-4.018c-2.219 0-4 1.781-4 4c0 2.219 1.781 4 4 4zM4.136 4.136L5.55 5.55m9.9 9.9l1.414 1.414M1.5 10.5h2m14 0h2M4.135 16.863L5.55 15.45m9.899-9.9l1.414-1.415M10.5 19.5v-2m0-14v-2"
+                                        opacity=".3"></path>
+                                    <g transform="translate(-210 -1)">
+                                        <path d="M220.5 2.5v2m6.5.5l-1.5 1.5"></path>
+                                        <circle cx="220.5" cy="11.5" r="4"></circle>
+                                        <path d="m214 5l1.5 1.5m5 14v-2m6.5-.5l-1.5-1.5M214 18l1.5-1.5m-4-5h2m14 0h2">
+                                        </path>
+                                    </g>
+                                </g>
+                            </svg>
+                            <div class="form-check form-switch fs-6">
+                                <input class="form-check-input  me-0" type="checkbox" id="toggle-dark"
+                                    style="cursor: pointer">
+                                <label class="form-check-label"></label>
+                            </div>
+                        </div>
+                        <div class="sidebar-toggler  x">
+                            <a href="#" class="sidebar-hide d-xl-none d-block"><i class="bi bi-x bi-middle"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="sidebar-menu">
+                    <ul class="menu">
+                        <li class="sidebar-title">Menu</li>
+
+                        <li class="sidebar-item active ">
+                            <a href="index.html" class='sidebar-link'>
+                                <i class="bi bi-grid-fill"></i>
+                                <span>Dashboard</span>
+                            </a>
+                        </li>
+
+                        <li class="sidebar-item  has-sub">
+                            <a href="#" class='sidebar-link'>
+                                <i class="bi bi-journal-check"></i>
+                                <span>Form Validation</span>
+                            </a>
+
+                            <ul class="submenu ">
+
+                                <li class="submenu-item  ">
+                                    <a href="form-validation-parsley.html" class="submenu-link">Parsley</a>
+
+                                </li>
+
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div id="main">
+            <header class="mb-3">
+                <a href="#" class="burger-btn d-block d-xl-none">
+                    <i class="bi bi-justify fs-3"></i>
+                </a>
+            </header>
+
+            <div class="page-heading">
+                <h3>Profile Statistics</h3>
+            </div>
+            <div class="page-content">
+                <section class="row">
+
+                </section>
+            </div>
+
+            <footer>
+                <div class="footer clearfix mb-0 text-muted">
+                    <div class="float-start">
+                        <p> &copy; 2025 team</p>
+                    </div>
+                </div>
+            </footer>
+        </div>
+    </div>
+    <script src="<?= base_url('template/client/') ?>static/js/components/dark.js"></script>
+    <script src="<?= base_url('template/client/') ?>extensions/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+
+
+    <script src="<?= base_url('template/client/') ?>compiled/js/app.js"></script>
+
+    <script src="<?= base_url('template/client') ?>/extensions/jquery/jquery.min.js"></script>
+    <?php
+        if(isset($js)){
+            foreach($js as $j){
+                echo '<script src="'.base_url('assets/js/client/').$j.'"></script>';
+            }
+        }
+    ?>
+
+</body>
+
+</html>
